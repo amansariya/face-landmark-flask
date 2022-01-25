aac8f636d7ffce5cb45dd280bbc7f7636d218180
Aman Sariya <amansariya@gmail.com>
2022-01-25 22:49:26 +0530

    added /uploads and optimised image reading

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import config
 import os
 from flask import Flask, flash, request, redirect, url_for, jsonify, render_template
 from werkzeug.utils import secure_filename
@@ -16,11 +17,10 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+app.config['SECRET_KEY'] = config.secret_key
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-
 
 def allowed_file(filename):
 	return '.' in filename and \
@@ -42,26 +42,32 @@ def upload_file():
 			flash('No file selected.')
 			return redirect(request.url)
 		if file and allowed_file(file.filename):
-			uploadFile = request.files['file']
-			image = cv2.imdecode(np.fromstring(uploadFile.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+			#uploadFile = request.files['file']
+			#image = cv2.imdecode(np.fromstring(uploadFile.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+			filename = secure_filename(file.filename)
+			pathToFile = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+			file.save(pathToFile)
+			image = cv2.imread(os.path.dirname(os.path.realpath(__file__))+"/uploads/"+filename)
 			
-			apply_model(image)
+			faceDetected = apply_model(image)
 
+			os.remove(pathToFile)
+			
 			output = True
 	
-	return render_template('landing.html', output = output, init = True)
+	return render_template('landing.html', output = output, faceDetected = faceDetected, init = True)
 
 def apply_model(image):
 	image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	x, y, width, depth = 50, 200, 950, 500
-	image_cropped = image_rgb
-	image_template = image_cropped.copy()
-	image_gray = cv2.cvtColor(image_cropped, cv2.COLOR_BGR2GRAY)
+	image_gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
 
 	haarcascade_url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_alt2.xml"
 	haarcascade = "haarcascade_frontalface_alt2.xml"
 	detector = cv2.CascadeClassifier(haarcascade)
 	faces = detector.detectMultiScale(image_gray)
+
+	if len(faces) == 0:
+		return False
 
 	LBFmodel_url = "https://github.com/kurnianggoro/GSOC2017/raw/master/data/lbfmodel.yaml"
 	LBFmodel = "lbfmodel.yaml"
@@ -73,12 +79,13 @@ def apply_model(image):
 
 	for landmark in landmarks:
 		for x,y in landmark[0]:
-			cv2.circle(image_cropped, (int(x), int(y)), 1, (255, 255, 255), 5)
+			cv2.circle(image_rgb, (int(x), int(y)), 1, (255, 255, 255), 20)
 
 	plt.axis("off")
-	plt.imshow(image_cropped)
+	plt.imshow(image_rgb)
 	fig.savefig('saved.png')
 	shutil.move("saved.png", "static/saved.png")
+	return True
 	
 if __name__ == "__main__":
 	app.run()
